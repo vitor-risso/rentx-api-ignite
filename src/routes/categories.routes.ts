a83d5a8a9632224abe1d16b31737a3222303af2b
vitor-risso <vitor.risso13@gmail.com>
2021-05-18 20:31:25 -0300

    [REFACTOR] changin the invocation

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer'
-import { createCategoryController } from '../modules/cars/useCases/createCategory/index'
+import createCategoryController from '../modules/cars/useCases/createCategory/index'
 import { importCategoryControlle } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
@@ -9,7 +9,7 @@ const categoriesRoutes = Router();
 const upload = multer({ dest: './tmp' })
 
 
-categoriesRoutes.post("/", (req, res) => createCategoryController.handle(req, res))
+categoriesRoutes.post("/", (req, res) => createCategoryController().handle(req, res))
 
 categoriesRoutes.get('/', (req, res) => listCategoriesController.handle(req, res))
 
