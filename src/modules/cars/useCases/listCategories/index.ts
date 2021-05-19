a13825038a8bc33c14ee2f76aa6a92241fc0fbc4
vitor-risso <vitor.risso13@gmail.com>
2021-05-19 14:04:09 -0300

    [REFACOTR] removing index because i've done the DI

--- src/modules/cars/useCases/listCategories/index.ts
+++
@@ -1,9 +0,0 @@
-import CategoriesRepository from "../../repositories/implementations/CategoriesRepository"
-import { ListCategoriesController } from "./ListaCategoriesController"
-import { ListCategoriesUseCase } from "./ListCategoriesUseCase"
-
-const repository = null
-const useCase = new ListCategoriesUseCase(repository)
-const listCategoriesController = new ListCategoriesController(useCase)
-
-export { listCategoriesController }
