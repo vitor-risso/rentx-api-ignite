b95beb24f248fb60aa17d358d74b816de628fbfb
vitor-risso <vitor.risso13@gmail.com>
2021-05-18 21:00:33 -0300

    [REFACTOR] changing the dirname

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -1,5 +1,5 @@
 import { getRepository, Repository } from "typeorm";
-import Category from "../../entitites/Category";
+import Category from "../../entities/Category";
 import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
 
 
