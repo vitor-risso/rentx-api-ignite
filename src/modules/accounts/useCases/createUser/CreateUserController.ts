f87d942ab056753e2954913f33d7fe36641c2a4f
vitor-risso <vitor.risso13@gmail.com>
2021-05-19 16:37:19 -0300

    [REFACTPR] addin try catch

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -6,17 +6,21 @@ import { CreateUserUseCase } from "./CreateUserUseCase";
 class CreateUserController {
 
   async handle(req: Request, res: Response): Promise<Response> {
-    const { name, password, email, driver_license } = req.body
-    const createUserUseCase = container.resolve(CreateUserUseCase)
+    try {
+      const { name, password, email, driver_license } = req.body
+      const createUserUseCase = container.resolve(CreateUserUseCase)
 
-    await createUserUseCase.execute({
-      name,
-      password,
-      email,
-      driver_license
-    })
+      await createUserUseCase.execute({
+        name,
+        password,
+        email,
+        driver_license
+      })
 
-    return res.status(201).send()
+      return res.status(201).send()
+    } catch (error) {
+      return res.status(400).send()
+    }
   }
 
 }
