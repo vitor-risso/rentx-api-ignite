ff4cae246d1ec621edb675677670003bc89c0212
vitor-risso <vitor.risso13@gmail.com>
2021-05-19 13:43:12 -0300

    [REFACTOR] refacotirng routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,21 @@
 import { Router } from 'express';
 import multer from 'multer'
 import { CreateCategoryController } from '../modules/cars/useCases/createCategory/CreateCategoryController';
-import { importCategoryControlle } from '../modules/cars/useCases/importCategory';
-import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import { ImportCategoryController } from '../modules/cars/useCases/importCategory/ImportCategoryController';
+import { ListCategoriesController } from '../modules/cars/useCases/listCategories/ListaCategoriesController';
 
 const categoriesRoutes = Router();
 
 const upload = multer({ dest: './tmp' })
 
 const createCategoryController = new CreateCategoryController()
+const importCategoryControlle = new ImportCategoryController()
+const listCategoriesController = new ListCategoriesController()
 
 categoriesRoutes.post("/", createCategoryController.handle)
 
-categoriesRoutes.get('/', (req, res) => listCategoriesController.handle(req, res))
+categoriesRoutes.get('/', listCategoriesController.handle)
 
-categoriesRoutes.post('/import', upload.single("file"), (req, res) => importCategoryControlle.handle(req, res))
+categoriesRoutes.post('/import', upload.single("file"), importCategoryControlle.handle)
 
 export default categoriesRoutes
