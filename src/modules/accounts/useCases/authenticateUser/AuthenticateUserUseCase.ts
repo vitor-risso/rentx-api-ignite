396c4ca38c07879870ef8108358e23e873fb20e7
vitor-risso <vitor.risso13@gmail.com>
2021-05-20 08:51:00 -0300

    [POST] creating a method to create a toke

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,54 @@
+import { compare } from "bcrypt"
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string
+}
+
+interface IResponse {
+  user: {
+    name: string,
+    email: string
+  };
+  token: string
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(@inject("UsersRepository")
+  private usersRepository: IUsersRepository) { }
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.finByEmail(email)
+
+    if (!user) {
+      throw new Error("Email or Password incorrect")
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new Error("Email or Password incorrect")
+    }
+
+    const token = sign({}, "93f54c8596aec8a37a6ed842ff7f98c6", {
+      subject: user.id,
+      expiresIn: "1d"
+    })
+
+    return {
+      user: {
+        email: user.email,
+        name: user.name
+      },
+      token
+    }
+
+  }
+}
+
+export { AuthenticateUserUseCase }
