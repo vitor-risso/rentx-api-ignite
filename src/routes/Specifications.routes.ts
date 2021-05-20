401a47870769cfee6ce5bde7bc584a8a0f5ece88
vitor-risso <vitor.risso13@gmail.com>
2021-05-20 09:11:31 -0300

    [MIDDLEWARES] addind a middleware for just a logined user create a specification

--- src/routes/Specifications.routes.ts
+++ src/routes/Specifications.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { Specification } from '../modules/cars/entities/Specification';
 import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
 
 const specificationsRoutes = Router();
 
 const specificatonController = new CreateSpecificationController()
+specificationsRoutes.use(ensureAuthenticated)
 specificationsRoutes.post("/", specificatonController.handle)
 
 
