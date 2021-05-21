5c534105de6fe0c0f6a1ec8069bd1cb5ddd57d2f
vitor-risso <vitor.risso13@gmail.com>
2021-05-21 11:40:31 -0300

    [CONFIG] creating fun to save files

---
+++ src/config/upload.ts
@@ -0,0 +1,23 @@
+import multer from 'multer';
+import crypto from 'crypto'
+
+import { resolve } from 'path'
+
+
+export default {
+
+  upload(folder: string) {
+    return {
+      storage: multer.diskStorage({
+        destination: resolve(__dirname, "..", "..", folder),
+        filename: (request, file, callback) => {
+          const fileHash = crypto.randomBytes(64).toString("hex");
+          const fileName = `${fileHash} - ${file.originalname}`;
+
+          return callback(null, fileName)
+        }
+      })
+    }
+
+  }
+}
