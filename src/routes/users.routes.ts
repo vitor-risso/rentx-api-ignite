d6a6494a41b3c883b6ee3cfbe68596ef6a047045
vitor-risso <vitor.risso13@gmail.com>
2021-05-21 11:40:13 -0300

    [ROUTES] adding route to create avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,23 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
+import uploadConfig from '../config/upload';
 
 const usersRoutes = Router()
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"))
+
 const createUserController = new CreateUserController()
+const updateUserAvatarController = new UpdateUserAvatarController()
 
 usersRoutes.post('/', createUserController.handle)
+usersRoutes.patch("/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+)
 
 export { usersRoutes }
