584f9abb642f9309fdd29c02a647a7c3c466586c
vitor-risso <vitor.risso13@gmail.com>
2021-05-25 13:57:20 -0300

    [INTERFACE] adding car repository interfae

---
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -0,0 +1,9 @@
+import { IcreateCartDTO } from "../dtos/ICreateCarDTO";
+import { Car } from "../infra/typeorm/entities/Car";
+
+interface ICarsRepository {
+  create(data: IcreateCartDTO): Promise<Car>
+  finByLicensePlate(license_plate: string): Promise<Car> | undefined;
+}
+
+export { ICarsRepository }
