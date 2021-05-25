b4f310427049bb9c9185cc5a1c325c3ecb18b7f4
vitor-risso <vitor.risso13@gmail.com>
2021-05-25 13:55:38 -0300

    [ENTITY] adding car entity

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,47 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid'
+
+@Entity('cars')
+class Car {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  dayli_rate: number;
+
+  @Column()
+  available: boolean;
+
+  @Column()
+  license_plate: string;
+
+  @Column()
+  fine_amount: number;
+
+  @Column()
+  brand: string;
+
+  @Column()
+  category_id: string;
+
+  @CreateDateColumn()
+  created_at: Date
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid()
+      this.available = true
+      this.created_at = new Date
+    }
+
+  }
+
+}
+
+export { Car }
