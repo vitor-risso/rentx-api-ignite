7889bbaf35d0d4b3cd6943eec0b95c4a5925abca
vitor-risso <vitor.risso13@gmail.com>
2021-05-25 13:55:20 -0300

    [DTO] create car dto

---
+++ src/modules/cars/dtos/ICreateCarDTO.ts
@@ -0,0 +1,13 @@
+
+interface IcreateCartDTO {
+  name: string;
+  description: string;
+  dayli_rate: number;
+  license_plate: string;
+  fine_amount: number;
+  brand: string;
+  category_id: string
+}
+
+
+export { IcreateCartDTO }
