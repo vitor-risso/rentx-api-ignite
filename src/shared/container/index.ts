6b8d7aafce882b7e703d666a2a45d969510bc7c9
vitor-risso <vitor.risso13@gmail.com>
2021-05-25 14:26:12 -0300

    [DI] addin car repository to container di

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,6 +2,8 @@ import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/Us
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository'
 import CategoriesRepository from '@modules/cars/infra/typeorm/repositories/CategoriesRepository'
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository'
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository'
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository'
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository'
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository'
 import { container } from 'tsyringe'
@@ -20,4 +22,9 @@ container.registerSingleton<ISpecificationsRepository>(
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
+)
+
+container.registerSingleton<ICarsRepository>(
+  "CarsRepository",
+  CarsRepository
 )
