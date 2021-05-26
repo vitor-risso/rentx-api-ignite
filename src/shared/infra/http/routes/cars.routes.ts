f15a4ede844af1a7bae160adc60471e5a923dbf4
vitor-risso <vitor.risso13@gmail.com>
2021-05-26 13:44:43 -0300

    [ROUTES] addin routes

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,4 +1,5 @@
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListCarsAvailableController } from "@modules/cars/useCases/listCarsAvailable/ListCarsAvailableController";
 import { Router } from "express";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -6,11 +7,15 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController()
+const listAvailableController = new ListCarsAvailableController()
 
 carsRoutes.post('/',
   ensureAuthenticated,
   ensureAdmin,
-  createCarController.hadle)
+  createCarController.hadle
+)
+
+carsRoutes.get('/available', listAvailableController.handle)
 
 export { carsRoutes };
 
