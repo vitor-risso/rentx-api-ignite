e0c96ec75c7537aa5878957a23ec5d8161c57b85
vitor-risso <vitor.risso13@gmail.com>
2021-05-26 11:21:59 -0300

    [REPOSITORY] updating repository

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -4,6 +4,7 @@ import { Car } from "../infra/typeorm/entities/Car";
 interface ICarsRepository {
   create(data: IcreateCartDTO): Promise<Car>
   finByLicensePlate(license_plate: string): Promise<Car> | undefined;
+  listAllCarsAvailable(category_id?: string, brand?: string, name?: string): Promise<Car[]> | undefined;
 }
 
 export { ICarsRepository }
