400a53207158536abfab239d098df869d6391321
vitor-risso <vitor.risso13@gmail.com>
2021-05-26 17:41:18 -0300

    [DI] addin repository to di

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,10 @@
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository'
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository'
+import { CarImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarImagesRepository'
+import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository'
 import CategoriesRepository from '@modules/cars/infra/typeorm/repositories/CategoriesRepository'
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository'
-import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository'
+import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository'
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository'
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository'
 import { ISpecificationsRepository } from '@modules/cars/repositories/ISpecificationsRepository'
@@ -27,4 +29,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<ICarsRepository>(
   "CarsRepository",
   CarsRepository
+)
+
+container.registerSingleton<ICarsImagesRepository>(
+  "CarImagesRepository",
+  CarImagesRepository
 )
