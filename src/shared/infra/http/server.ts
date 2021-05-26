a985ae489f412fdf752719136707f4ad937f7f9b
vitor-risso <vitor.risso13@gmail.com>
2021-05-26 09:25:54 -0300

    [ADMIN] creating admin into users db

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,17 +1,17 @@
-import "reflect-metadata"
 import express, { NextFunction, Request, Response } from 'express';
-import "express-async-errors"
-
-import '../typeorm'
-import '../../container'
-
-import { router } from './routes';
+import "express-async-errors";
+import "reflect-metadata";
 import swaggerUi from 'swagger-ui-express';
-
 import swaggerFile from '../../../swagger.json';
+import '../../container';
 import { AppError } from '../../errors/AppError';
+import createConnection from '../typeorm';
+import { router } from './routes';
 
 
+
+
+createConnection()
 const app = express();
 
 app.use(express.json());
---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,20 @@
+import { hash } from 'bcrypt';
+import { v4 as uuid } from 'uuid';
+import createConnection from '../index';
+async function create() {
+  const connection = await createConnection("localhost");
+
+  const id = uuid()
+  const password = await hash("admin", 8)
+
+  await connection.query(
+    `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+    values('${id}', 'admin', 'admin@admin.com.br','${password}' , true, 'now()', 'XXXXX')
+    `
+  )
+
+  await connection.close()
+}
+
+
+create().then(() => console.log('OK'))
