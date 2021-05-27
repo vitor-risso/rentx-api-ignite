ff77f4bd314b00d4fdbaa0e78fc3eccc73aa476c
vitor-risso <vitor.risso13@gmail.com>
2021-05-27 11:00:11 -0300

    [IREPOSITORY] adding function create

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -1,8 +1,11 @@
+import { ICreateRentalDTO } from "../dtos/ICreateRentalDTO";
 import { Rental } from "../infra/typeorm/entities/Rental";
+
 
 interface IRentalsRepository {
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
+  create({ user_id, car_id, expected_return_date }: ICreateRentalDTO): Promise<Rental>;
 }
 
 export { IRentalsRepository };
