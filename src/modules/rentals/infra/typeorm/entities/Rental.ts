9c902a002b55039b8fd23b12e277bc7b4098f1ed
vitor-risso <vitor.risso13@gmail.com>
2021-05-27 16:47:13 -0300

    [ENTITY] addin typeorm to entity rental

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,24 +1,33 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
-
+@Entity("rentals")
 class Rental {
 
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   car_id: string;
 
+  @Column()
   user_id: string;
 
+  @Column()
   start_date: Date;
 
+  @Column()
   end_date: Date;
 
-  expect_return_date: Date;
+  @Column()
+  expected_return_date: Date;
 
   total: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 
   constructor() {
