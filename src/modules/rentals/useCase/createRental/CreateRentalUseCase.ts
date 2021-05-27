dbfe904db8f820ea66bc57376d930bcbdfcb98e6
vitor-risso <vitor.risso13@gmail.com>
2021-05-27 16:47:59 -0300

    [DI] adding di to use case

--- src/modules/rentals/useCase/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCase/createRental/CreateRentalUseCase.ts
@@ -2,6 +2,7 @@ import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental"
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository"
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider"
 import { AppError } from "@shared/errors/AppError"
+import { inject, injectable } from "tsyringe"
 
 
 
@@ -11,11 +12,13 @@ interface IRequest {
   expected_return_date: Date;
 }
 
+@injectable()
 class CreateRentalUseCase {
 
   constructor(
-
+    @inject("RentalsRepository")
     private rentalCarRepository: IRentalsRepository,
+    @inject("DayjsDateProvider")
     private dateProvider: IDateProvider
   ) { }
 
