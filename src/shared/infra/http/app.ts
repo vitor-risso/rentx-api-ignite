0a7fe5891274e980767658fc482835846e452213
vitor-risso <vitor.risso13@gmail.com>
2021-05-28 14:13:47 -0300

    [SERVER] separationg server from app

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,36 @@
+import express, { NextFunction, Request, Response } from 'express';
+import "express-async-errors";
+import "reflect-metadata";
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../../../swagger.json';
+import '../../container';
+import { AppError } from '../../errors/AppError';
+import createConnection from '../typeorm';
+import { router } from './routes';
+
+
+
+
+createConnection()
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.use(router)
+
+app.use((e: Error, req: Request, res: Response, next: NextFunction) => {
+  if (e instanceof AppError) {
+    return res.status(e.statusCode).json({
+      message: e.message
+    })
+  }
+
+  return res.status(500).json({
+    error: "error",
+    status: `Internal server error - ${e.message}`
+  })
+})
+
+export { app }
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,37 +1,4 @@
-import express, { NextFunction, Request, Response } from 'express';
-import "express-async-errors";
-import "reflect-metadata";
-import swaggerUi from 'swagger-ui-express';
-import swaggerFile from '../../../swagger.json';
-import '../../container';
-import { AppError } from '../../errors/AppError';
-import createConnection from '../typeorm';
-import { router } from './routes';
-
-
-
-
-createConnection()
-const app = express();
-
-app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
-
-app.use(router)
-
-app.use((e: Error, req: Request, res: Response, next: NextFunction) => {
-  if (e instanceof AppError) {
-    return res.status(e.statusCode).json({
-      message: e.message
-    })
-  }
-
-  return res.status(500).json({
-    error: "error",
-    status: `Internal server error - ${e.message}`
-  })
-})
+import { app } from "./app";
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080")
