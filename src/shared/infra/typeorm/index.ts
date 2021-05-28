bc57e4d7cd75bdd24759e3836069756a1208f7da
vitor-risso <vitor.risso13@gmail.com>
2021-05-28 14:12:18 -0300

    [DB] updatting connection with db

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -18,7 +18,8 @@ export default async (host = "database_ignite"): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host
+      host: process.env.NODE_ENV === 'test' ? "localhost" : host,
+      database: process.env.NODE_ENV === 'test' ? "rentx_test" : defaultOptions.database
     })
   )
 }
