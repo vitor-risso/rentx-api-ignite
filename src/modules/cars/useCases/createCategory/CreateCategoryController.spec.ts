c106cf6f0803b0a4e966b7d15edf16ee2a2a8353
vitor-risso <vitor.risso13@gmail.com>
2021-05-28 14:13:19 -0300

    [TEST] first integration test

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,54 @@
+import { app } from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+import { hash } from 'bcrypt';
+import request from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+
+let connection: Connection
+
+describe('Create Category Controller', () => {
+
+  beforeAll(async () => {
+    connection = await createConnection()
+    await connection.runMigrations()
+    const id = uuid()
+    const password = await hash("admin", 8)
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+    values('${id}', 'admin', 'admin@admin.com.br','${password}' , true, 'now()', 'XXXXX')
+    `
+    )
+  })
+
+  afterAll(async () => {
+    await connection.dropDatabase()
+    await connection.close()
+  })
+
+  it("Should able to create a new category", async () => {
+    const responseToken = await request(app).post('/session').send({
+      email: "admin@admin.com.br",
+      password: "admin"
+    })
+
+    const { token } = responseToken.body
+
+    const response = await request(app).post('/categories')
+      .send({
+        name: "Category test",
+        description: "Description test"
+      }).set({
+        Authorization: ``
+      })
+
+    expect(response.statusCode).toBe(201)
+    expect(response.body).toBe({
+      name: "Category test",
+      description: "Description test"
+    })
+  })
+
+})
