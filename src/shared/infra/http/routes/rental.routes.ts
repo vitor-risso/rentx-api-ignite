f37c05a357d4a5617be534f30844e9eaa6b84dbf
vitor-risso <vitor.risso13@gmail.com>
2021-06-08 15:21:12 -0300

    [ROUTES] updating rental routes

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,4 +1,5 @@
 import { CreateRentalController } from "@modules/rentals/useCase/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCase/devolutionRental/DevolutionRentalController";
 import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
@@ -6,8 +7,10 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const rentalRoutes = Router()
 
 const createRentalController = new CreateRentalController()
+const devoitionRentalController = new DevolutionRentalController()
 
 rentalRoutes.post('/', ensureAuthenticated, createRentalController.handle)
+rentalRoutes.post('/devolution/:id', ensureAuthenticated, devoitionRentalController.handle)
 
 
 export { rentalRoutes };
