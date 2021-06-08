d0437a498b7ae03d682794f8bf8519f6b5b8679d
vitor-risso <vitor.risso13@gmail.com>
2021-06-08 13:10:18 -0300

    [REPOSITORY] adding function

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -6,6 +6,7 @@ interface ICarsRepository {
   finByLicensePlate(license_plate: string): Promise<Car> | undefined;
   listAllCarsAvailable(category_id?: string, brand?: string, name?: string): Promise<Car[]> | undefined;
   findById(car_id: string): Promise<Car> | undefined;
+  updateAvailable(id: string, available: boolean): Promise<void>
 }
 
 export { ICarsRepository }
