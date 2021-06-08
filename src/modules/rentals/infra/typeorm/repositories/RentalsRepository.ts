69c55bedb47f1de872c78eae280735bc46ad51ae
vitor-risso <vitor.risso13@gmail.com>
2021-06-08 15:19:35 -0300

    [REPOSITORY] updating rental

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -12,18 +12,28 @@ class RentalsRepository implements IRentalsRepository {
   }
 
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    return await this.repository.findOne({ car_id })
+    return await this.repository.findOne({ where: { car_id, end_date: null } })
   }
 
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    return await this.repository.findOne({ user_id })
+    return await this.repository.findOne({ where: { user_id, end_date: null } })
   }
 
-  async create({ user_id, car_id, expected_return_date }: ICreateRentalDTO): Promise<Rental> {
+  async create({
+    user_id,
+    car_id,
+    expected_return_date,
+    id,
+    end_date,
+    total
+  }: ICreateRentalDTO): Promise<Rental> {
     const rental = this.repository.create({
       car_id,
       expected_return_date,
-      user_id
+      user_id,
+      id,
+      end_date,
+      total
     })
 
     await this.repository.save(rental)
