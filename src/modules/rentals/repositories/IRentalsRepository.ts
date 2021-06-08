c2cdcadb9df7c3fa20618637a5c6dfd32f80b5da
vitor-risso <vitor.risso13@gmail.com>
2021-06-08 14:37:08 -0300

    [REPOSITORY] addin funciton to rental repository

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -6,6 +6,7 @@ interface IRentalsRepository {
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
   create({ user_id, car_id, expected_return_date }: ICreateRentalDTO): Promise<Rental>;
+  findById(id: string): Promise<Rental | undefined>
 }
 
 export { IRentalsRepository };
