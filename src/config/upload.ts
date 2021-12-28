d23a604859e458d8f0ee26e2f898fae13e14d290
Vitor Risso <vrisso@lifescan.com>
2021-12-28 19:01:37 -0300

    refactor

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,23 +1,19 @@
-import multer from 'multer';
-import crypto from 'crypto'
+import multer from "multer";
+import crypto from "crypto";
 
-import { resolve } from 'path'
+import { resolve } from "path";
 
+const tmpFolder = resolve(__dirname, "..", "..", "tmp");
 
 export default {
+  tmpFolder,
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename: (request, file, callback) => {
+      const fileHash = crypto.randomBytes(64).toString("hex");
+      const fileName = `${fileHash} - ${file.originalname}`;
 
-  upload(folder: string) {
-    return {
-      storage: multer.diskStorage({
-        destination: resolve(__dirname, "..", "..", folder),
-        filename: (request, file, callback) => {
-          const fileHash = crypto.randomBytes(64).toString("hex");
-          const fileName = `${fileHash} - ${file.originalname}`;
-
-          return callback(null, fileName)
-        }
-      })
-    }
-
-  }
-}
+      return callback(null, fileName);
+    },
+  }),
+};
