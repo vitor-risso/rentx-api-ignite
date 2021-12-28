cd997aa990da3bd6cb67c28e18a272ef17f9f276
Vitor Risso <vrisso@lifescan.com>
2021-12-28 19:02:09 -0300

    update update avatar use case

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,7 +1,6 @@
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { inject, injectable } from "tsyringe";
-import { deleteFile } from "@utils/file";
-
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
   user_id: string;
@@ -10,26 +9,26 @@ interface IRequest {
 
 @injectable()
 class UpdateUserAvatarUseCase {
-
   constructor(
     @inject("UsersRepository")
-    private repository: IUsersRepository
-  ) { }
+    private repository: IUsersRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
+  ) {}
 
   async execute({ user_id, avatar_file }: IRequest): Promise<void> {
-
-    const user = await this.repository.findById(user_id)
+    const user = await this.repository.findById(user_id);
 
     if (user.avatar) {
-      await deleteFile(`./tmp/avatar/${user.avatar}`)
+      await this.storageProvider.delete(user.avatar, "avatar");
     }
+
+    await this.storageProvider.save(avatar_file, "avatar");
 
     user.avatar = avatar_file;
 
-    await this.repository.create(user)
-
+    await this.repository.create(user);
   }
 }
 
 export { UpdateUserAvatarUseCase };
-
